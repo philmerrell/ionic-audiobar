e7a4eec1e3a06ee27d97cb1d81421209712e7269
Phil Merrell <philmerrell@boisestate.edu>
2017-02-03 16:48:24 -0700

    refactor: Update demo

--- demo/src/pages/home/home.ts
+++ demo/src/pages/home/home.ts
@@ -9,7 +9,7 @@ import { Track } from 'ionic-audiobar';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public myPlaylist: Track[] = [
+  public myPlaylist = [
       new Track('https://api.soundcloud.com/tracks/62188129/stream?client_id=df942240e3e63f8e23596df0893eab2a', 'Mac Demarco', 'Ode to Viceroy', 'https://i1.sndcdn.com/artworks-000031540797-8io3vh-t500x500.jpg')
     ];
   constructor(public navCtrl: NavController) {
